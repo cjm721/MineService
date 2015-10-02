21520fb1b7eae1ada4f6d3f299baa0a121c7e30e
CJ Miller <millerc5@rose-hulman.edu>
2015-10-02 14:01:59 -0400

    Initial Commit

---
+++ MineServiceClient/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MineServiceClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tcpConnectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tcpConnectButton
+            // 
+            this.tcpConnectButton.Location = new System.Drawing.Point(13, 13);
+            this.tcpConnectButton.Name = "tcpConnectButton";
+            this.tcpConnectButton.Size = new System.Drawing.Size(121, 23);
+            this.tcpConnectButton.TabIndex = 0;
+            this.tcpConnectButton.Text = "TCP Connect";
+            this.tcpConnectButton.UseVisualStyleBackColor = true;
+            this.tcpConnectButton.Click += new System.EventHandler(this.tcpConnectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.tcpConnectButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tcpConnectButton;
+    }
+}
+
---
+++ MineServiceClient/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineServiceClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tcpConnectButton_Click(object sender, EventArgs e)
+        {
+            TcpClient clientSocket = new TcpClient();
+            clientSocket.Connect("127.0.0.1", 56552);
+
+
+            StreamReader reader = new StreamReader(clientSocket.GetStream(), Encoding.ASCII);
+            StreamWriter writer = new StreamWriter(clientSocket.GetStream(), Encoding.ASCII);
+            string line;
+            line = reader.ReadLine();
+            Console.WriteLine("Message: " + line);
+            writer.WriteLine("I Recived: " + line);
+            writer.Flush();
+        }
+    }
+}
