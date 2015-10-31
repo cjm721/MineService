db46c1c3b373ef1975e9fd310cdce48c9a1f9282
Ishank <ishank.tandon@hotmail.com>
2015-10-31 10:27:39 -0400

    resolved merge conflicts

--- WPFMineServiceTest/MainWindow.xaml.cs
+++ WPFMineServiceTest/MainWindow.xaml.cs
@@ -16,6 +16,9 @@ using MineService_Client_JSON;
 using Newtonsoft.Json;
 using System.IO;
 using System.Net.Sockets;
+using System.Windows.Markup;
+using System.Xml;
+
 
 namespace WPFMineServiceTest
 {
@@ -37,7 +40,7 @@ namespace WPFMineServiceTest
                 getData(temp);
                 currentTabName = temp.Name;
             };
-           
+
 
             server_TabControl.SelectionChanged += (o, e) =>
             {
@@ -84,40 +87,98 @@ namespace WPFMineServiceTest
 
         private void start_stop_button_Click(object sender, RoutedEventArgs e)
         {
-            //if(currentTabName.Contains("server_Tab_"))
-            //{
-            System.Console.WriteLine("DEBUG: Gets here");
-            Button button = ((Button)sender);
-            String server = server_name_TextBlock.Text;
-            States.MCCommandTYPE state = States.MCCommandTYPE.Start;
-
-            if (((Button)sender).Content.ToString().Contains("Stop"))
+            if (server_name_TextBlock.Name.Contains("server_Tab_"))
             {
-                state = States.MCCommandTYPE.Stop;
+                System.Console.WriteLine("DEBUG: Gets here");
+                Button button = ((Button)sender);
+                String server = server_name_TextBlock.Text;
+                States.MCCommandTYPE state = States.MCCommandTYPE.Start;
+
+                if (((Button)sender).Content.ToString().Contains("Stop"))
+                {
+                    state = States.MCCommandTYPE.Stop;
+                }
+                else if (((Button)sender).Content.ToString().Contains("Start"))
+                {
+                    state = States.MCCommandTYPE.Start;
+                }
+
+                MCCommand command = new MCCommand(state, server, "");
+                System.Console.WriteLine("command: " + command.type);
+                String json = JsonConvert.SerializeObject(command);
+                Message msg = new Message(States.MessageTYPE.MCCommand, json);
+                String js = JsonConvert.SerializeObject(msg);
+                System.Console.WriteLine("before sending to server, js: " + js.ToString());
+                CommunicationClient.INSTANCE.sendToServer(js);
+                System.Console.WriteLine("after send to server");
+
+                button.Content = "Pending";
+                button.IsEnabled = false;
+
+                button.Content = (state == States.MCCommandTYPE.Start) ? "Stop" : "Start";
+                button.IsEnabled = true;
+
+
             }
-            else if (((Button)sender).Content.ToString().Contains("Start"))
-            {
-                state = States.MCCommandTYPE.Start;
-            }
-
-            MCCommand command = new MCCommand(state, server, "");
-            System.Console.WriteLine("command: " + command.type);
-            String json = JsonConvert.SerializeObject(command);
-            Message msg = new Message(States.MessageTYPE.MCCommand, json);
-            String js = JsonConvert.SerializeObject(msg);
-            System.Console.WriteLine("before sending to server, js: " + js.ToString());
-            CommunicationClient.INSTANCE.sendToServer(js);
-            System.Console.WriteLine("after send to server");
-
-            button.Content = "Pending";
-            button.IsEnabled = false;
-
-            button.Content = (state == States.MCCommandTYPE.Start) ? "Stop" : "Start";
-            button.IsEnabled = true;
-            
-            
-            //}
 
         }
+
+
+        private void add_new_server_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(new_server_folder.Text) || String.IsNullOrWhiteSpace(new_server_name.Text))
+            {
+                if (String.IsNullOrWhiteSpace(new_server_name.Text) && String.IsNullOrWhiteSpace(new_server_folder.Text))
+                {
+                    MessageBox.Show("Must enter server name and server file", "Required Field Missing", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else if (String.IsNullOrWhiteSpace(new_server_name.Text))
+                {
+                    MessageBox.Show("Must enter server name", "Required Field Missing", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Must enter server folder", "Required Field Missing", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                return;
+            }
+            TabItem new_tab = TrycloneElement(server_Tab_1);
+            //TabItem new_tab = GetNewServerTabItem();
+            if (new_tab != null)
+            {
+                cluster_TabControl.Items.Add(new_tab);
+                cluster_TabControl.Items.Remove(add_new_server); //These make sure that the add new server tab is always at the bottom
+                cluster_TabControl.Items.Add(add_new_server);
+            }
+        }
+
+        public static T TrycloneElement<T>(T orig)
+        {
+            try
+            {
+                string s = XamlWriter.Save(orig);
+                StringReader stringReader = new StringReader(s);
+                XmlReader xmlReader = XmlTextReader.Create(stringReader, new XmlReaderSettings());
+                XmlReaderSettings sx = new XmlReaderSettings();
+                object x = XamlReader.Load(xmlReader);
+                return (T)x;
+            }
+            catch
+            {
+                return (T)((object)null);
+            }
+        }
+
+        //public static T GetNewServerTabItem<T>()
+        //{
+        //    string s = XamlWriter.Save(File.ReadAllText("newServerTab.xaml.new"));
+        //    StringReader stringReader = new StringReader(s);
+        //    XmlReader xmlReader = XmlTextReader.Create(stringReader, new XmlReaderSettings());
+        //    XmlReaderSettings sx = new XmlReaderSettings();
+        //    object x = XamlReader.Load(xmlReader);
+        //    return (T)x;
+        //    //return  (TabItem) XamlReader.Parse(File.ReadAllText("newServerTab.xaml.new"));
+        //}
+
     }
 }
