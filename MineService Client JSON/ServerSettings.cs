45414e503ac1160b19880e51f57f3557c07838cb
Max Morgan <maxmorgan94@gmail.com>
2015-11-05 00:02:48 -0500

    Added a new class specifically for the settings for a unique server. ~Max

---
+++ MineService Client JSON/ServerSettings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineService_Client_JSON
+{
+    public class ServerSettings
+    {
+        public bool enable_rcon;
+        public bool white_list;
+        public int spawn_protection;
+        public int max_tick_time;
+        public string generator_settings;
+        public bool force_gamemode;
+        public bool allow_nether;
+        public int gamemode;
+        public bool enable_query;
+        public int player_idle_timeout;
+        public int difficult;
+        public bool spawn_monsters;
+        public int op_permission_level;
+        public string resource_pack_hash;
+        public bool announce_player_achievements;
+        public bool pvp;
+        public bool snooper_enabled;
+        public string level_type;
+        public bool hardcore;
+        public bool enable_command_block;
+        public int max_players;
+        public int network_compression_threshold;
+        public int max_world_size;
+        public int server_port;
+        public string server_ip;
+        public bool spawn_npcs;
+        public bool allow_flight;
+        public string level_name;
+        public int view_distance;
+        public string resource_pack;
+        public bool spawn_animals;
+        public bool generate_structures;
+        public bool online_mode;
+        public int max_build_height;
+        public string level_seed;
+        public string motd;
+        public bool enable_rcon;
+    }
+}
