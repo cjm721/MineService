d88219230b493c924e3ae9e1a592c46fd6457536
CJ Miller <cjmiller721@gmail.com>
2015-12-16 07:22:32 -0500

    Making the exported files relate to the project.

--- WPFMineServiceTest/Properties/AssemblyInfo.cs
+++ WPFMineServiceTest/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WPFMineServiceTest")]
+[assembly: AssemblyTitle("MineService Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WPFMineServiceTest")]
+[assembly: AssemblyProduct("MineService")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
