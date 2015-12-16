e32a2f0f4b8045bd589eab7573bcbf6ec63337ee
Brandon Hennessey <hennesbm@rose-hulman.edu>
2015-12-16 10:25:21 -0500

    Added JSONConvertTest method

--- UnitTestProject1/MainWindowTest.cs
+++ UnitTestProject1/MainWindowTest.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1
 {
-    class MainWindowTest
+    [TestClass]
+    public class MainWindowTest
     {
+        [TestMethod]
+        public void JSONConvertTest()
+        {
+
+        }
     }
 }
