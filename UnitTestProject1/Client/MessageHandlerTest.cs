cff1cbd0c36cb6a0ec3e7c2b1a3513000400b78a
Collin Trowbridge <trowbrct@rose-hulman.edu>
2016-01-27 10:49:14 -0500

    Adjust MessangeHandlerTests

--- UnitTestProject1/Client/MessageHandlerTest.cs
+++ UnitTestProject1/Client/MessageHandlerTest.cs
@@ -11,6 +11,11 @@ using MineService_JSON;
 using System.Windows.Controls;
 using System.Net.Sockets;
 using System.IO;
+using System.Collections;
+using Newtonsoft.Json;
+using System.Windows.Documents;
+using MineService_Shared.Json;
+using System.Threading;
 
 namespace UnitTestProject1
 {
@@ -77,21 +82,37 @@ namespace UnitTestProject1
             Assert.AreEqual(5, tabCount);
             Assert.AreEqual(2, Data.serverTabs.Count);
         }
-    
-        /*
+        
         [TestMethod]
         public void TestHandleNewWindow()
         {
-            MethodInfo methodInfo = typeof(CommunicationClient).GetMethod("handleNewWindow", BindingFlags.NonPublic | BindingFlags.Instance);
+            Status[] status = new Status[] {
+                new Status(States.StatusType.Send, "999", new ServerStatus(true, 1000)),
+                new Status(States.StatusType.Send, "21", new ServerStatus(false, 1000))
+            };
+            StatusArray statuses = new StatusArray(status);
 
-            String message = "[{\"TYPE\":\"Send\",\"ServerID\":\"hello\",\"serverStatus\":{\"isRunning\":false,\"uptime\":0,\"settings\":{\"enable_rcon\":false,\"white_list\":false,\"spawn_protection\":0,\"max_tick_time\":0,\"generator_settings\":null,\"force_gamemode\":false,\"allow_nether\":false,\"gamemode\":0,\"enable_query\":false,\"player_idle_timeout\":0,\"difficulty\":0,\"spawn_monsters\":false,\"op_permission_level\":0,\"resource_pack_hash\":null,\"announce_player_achievements\":false,\"pvp\":false,\"snooper_enabled\":false,\"level_type\":null,\"hardcore\":false,\"enable_command_block\":false,\"max_players\":0,\"network_compression_threshold\":0,\"max_world_size\":0,\"server_port\":0,\"server_ip\":null,\"spawn_npcs\":false,\"allow_flight\":false,\"level_name\":null,\"view_distance\":0,\"spawn_animals\":false,\"generate_structures\":false,\"online_mode\":false,\"max_build_height\":0,\"level_seed\":null,\"motd\":null}}}]";
-            Message msg = new Message(States.MessageTYPE.Login | States.MessageTYPE.Error, message);
+            new LoginWindow().Show();
+            MethodInfo methodInfo = typeof(MessageHandler).GetMethod("handleNewWindow", BindingFlags.NonPublic | BindingFlags.Instance);
+            
+            FieldInfo mainWindowClusterTab = typeof(MainWindow).GetField("cluster_TabControl", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            TabControl control = (TabControl)mainWindowClusterTab.GetValue(window);
 
-            Assert.AreEqual(0, Data.serverTabs.Count);
-            methodInfo.Invoke(client, new Object[] { msg });
-            Assert.AreNotEqual(0, Data.serverTabs.Count);
+            int tabCount = control.Items.Count;
+
+            Assert.AreEqual(3, tabCount);
+            Thread thread = new Thread(delegate() { methodInfo.Invoke(handler, new Object[] { statuses }); });
+            thread.Start();
+            thread.Join();
+
+            
+            LoginWindow.INSTANCE.Dispatcher.Invoke(new Action(delegate()
+            {
+                tabCount = control.Items.Count;
+                Assert.AreNotEqual(3, tabCount);
+            }));
         }
-        */
+        
 
         [TestCleanup]
         public void tearDown()
